308dd9bf8e694ff6e4b7f62c45652c4ae0570f76
evgen4ikrus <67272160+evgen4ikrus@users.noreply.github.com>
2022-09-04 17:51:29 +0300

    add timestamp to params

--- main.py
+++ main.py
@@ -5,17 +5,34 @@ from time import sleep
 
 
 if __name__ == '__main__':
+
     load_dotenv()
     devman_token = os.environ['DEVMAN_TOKEN']
+
     url = 'https://dvmn.org/api/long_polling/'
     headers = {
         'Authorization': f'Token {devman_token}'
     }
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+    new_checks = response.json()
+    if new_checks['status'] == 'timeout':
+        timestamp = new_checks['timestamp_to_request']
+    else:
+        print(new_checks)
+        timestamp = new_checks['last_attempt_timestamp']
+
     while True:
+
         try:
-            response = requests.get(url, headers=headers, timeout=5)
+            params = {
+                'timestamp': timestamp
+            }
+            response = requests.get(url, headers=headers, params=params, timeout=5)
             response.raise_for_status()
             print(response.json())
+            timestamp = response.json()['last_attempt_timestamp']
+
         except requests.exceptions.ReadTimeout:
             print('Обновлений нет')
         except requests.exceptions.ConnectionError:
